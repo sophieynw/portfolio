6c35706e036582ffea84c167bf7ddff1f68d2eff
sophieynw <wang.sophie@live.ca>
2024-12-22 18:31:52 -0500

    Added vercel.json file for vercel deployment.

--- app.js
+++ app.js
@@ -16,24 +16,3 @@ app.get("/story", controller.renderStory);
 app.listen(process.env.PORT || 3000, () => {
   console.log(`Server running on port ${port}`);
 });
-
-
-const path = require('path');
-const fs = require('fs');
-
-app.get('/debug', (req, res) => {
-  try {
-    const homePath = path.join(__dirname, 'data/home.json');
-    const homeData = JSON.parse(fs.readFileSync(homePath, 'utf-8'));
-    res.send({
-      message: 'Debugging successful!',
-      homePath,
-      homeData,
-    });
-  } catch (err) {
-    res.status(500).send({
-      message: 'Error in debug route',
-      error: err.message,
-    });
-  }
-});
